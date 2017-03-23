ce5af89f77b0683b55b3b8c4c6425664afc2cfc0
Mats Rauhala <mats.rauhala@iki.fi>
2017-03-23 15:34:45 +0200

    User data

--- src/Data/IRC.hs
+++ src/Data/IRC.hs
@@ -11,10 +11,15 @@ import Data.Aeson
 import GHC.Generics
 import Data.String
 import Control.Lens
+import Data.SafeCopy
+import Data.Time
 
-newtype Channel = Channel Text deriving (Generic, Show, ToJSON, FromJSON, IsString)
-newtype Nick = Nick Text deriving (Generic, Show, ToJSON, FromJSON, IsString)
-newtype Message = Message Text deriving (Generic, Show, ToJSON, FromJSON, IsString)
+newtype Channel = Channel Text deriving (Generic, Show, ToJSON, FromJSON, IsString, Ord, Eq)
+newtype Nick = Nick Text deriving (Generic, Show, ToJSON, FromJSON, IsString, Ord, Eq)
+
+data Message = Message { messageTimestamp :: UTCTime
+                       , messageContent :: Text }
+             deriving (Show, Generic)
 
 data IrcMessage = IrcMessage { ircMessageSource :: Either Channel Nick
                              , ircMessageMessage :: Message }
@@ -31,9 +36,16 @@ data IrcEvent = PrivateMessage IrcMessage
               deriving (Generic, Show)
 
 makeFields ''IrcMessage
+makeFields ''Message
+makeFields ''IrcJoin
 makePrisms ''IrcEvent
+deriveSafeCopy 0 'base ''Nick
+deriveSafeCopy 0 'base ''Channel
+deriveSafeCopy 0 'base ''Message
 
 
+instance ToJSON Message
+instance FromJSON Message
 instance ToJSON IrcJoin
 instance FromJSON IrcJoin
 instance ToJSON IrcMessage
---
+++ src/Data/User.hs
@@ -0,0 +1,26 @@
+{-# Language TemplateHaskell #-}
+{-# Language MultiParamTypeClasses #-}
+{-# Language FunctionalDependencies #-}
+{-# Language FlexibleInstances #-}
+module Data.User where
+
+import Data.IRC (Nick, Channel, Message)
+import Data.Text (Text)
+import Control.Lens
+import Data.ByteString (ByteString)
+import Data.Set (Set)
+import Data.SafeCopy
+import Data.Sequence (Seq)
+import Data.Map.Strict (Map)
+
+data User = User { userEmail :: Text
+                 , userPreferredNick :: Nick
+                 , userPassword :: ByteString
+                 , userChannels :: Set Channel
+                 , userHistory :: Map Channel Message
+                 }
+          deriving (Show)
+
+deriveSafeCopy 0 'base ''User
+
+makeFields ''User
